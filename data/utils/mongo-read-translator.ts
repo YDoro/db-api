12b4bd040271fbc8cbd430163c97748e72d1ce0a
yan <yan_mvd@hotmail.com>
2024-06-26 12:42:16 -0300

    feat: ✨ create a mongo request parser

---
+++ data/utils/mongo-read-translator.ts
@@ -0,0 +1,50 @@
+import { ObjectId } from "mongodb";
+import type { Request } from "../../interfaces/presentation";
+
+type query = {
+    collection:string,
+    pipeline?:any[]
+}
+
+export default (req:Request): query => {
+    const {url,query} = req
+    const [col,...rest] = url.slice(1).split("/")
+    
+    const pipelines:any[] = [];
+
+    rest.filter(v=>v).forEach((segment,i,rest) => {
+
+        if (ObjectId.isValid(segment)){
+            pipelines.push({
+                $match:{
+                    "_id": new ObjectId(segment)
+                }
+            })
+        } else {
+            pipelines.push(
+                {
+                    $unwind:`$${segment}`
+                },
+                {
+                    $replaceRoot: {
+                        newRoot: `$${segment}`
+                    }
+                }
+            )
+        }
+    })
+    
+    const match:any = {}
+    // TODO - imporve types - query including types
+    query?.split("&").forEach((arg)=>{
+        const [param,value] = arg.split("=")
+        match[`${param}`] = value
+        
+    })
+
+    pipelines.push({
+        $match: match
+    })
+
+    return {collection:col,pipeline:pipelines}
+}
