f9499026f0d91770f080ec05900926e8bf904e13
yan <yan_mvd@hotmail.com>
2024-07-05 18:20:02 -0300

    feat: :sparkles: exposing update handler

--- routes/documents.ts
+++ routes/documents.ts
@@ -1,8 +1,11 @@
 import type { Router } from "express";
 import adapt from "../adapters/express/adapt-express-routes";
-import { HandleDocumentCreation, HandleDocumentRead } from "../handlers/document";
+import { HandleDocumentCreation, HandleDocumentRead, HandleDocumentUpdate } from "../handlers/document";
 
 export default (router: Router) => {
     router.get("/*", adapt(HandleDocumentRead))
     router.post("/*", adapt(HandleDocumentCreation))
+    router.put("/*",adapt(HandleDocumentUpdate))
+    //TODO - ensure to just update fields that exists in patch and replace on put
+    router.patch("/*",adapt(HandleDocumentUpdate))
 }
