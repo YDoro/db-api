b69b864889ae32854b227596174932e18de4d612
yan <yan_mvd@hotmail.com>
2024-12-06 12:16:25 -0300

    feat:  🚧 create a cache middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,8 @@
 import type { Express } from "express";
 import express from "express";
+import CacheMiddleware from "../middlewares/express/cache";
 
 export default (app: Express) => {
     app.use(express.json());
+    app.use(CacheMiddleware);
 };
---
+++ middlewares/express/cache.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from "express";
+import { getClient } from "../../config/cache";
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (req.method === "GET") {
+        const r = await getClient();
+        const auth = req.headers?.authorization || "";
+        const cached = await r.get(req.url + auth);
+
+        if (cached) {
+            const cachedResponse = JSON.parse(cached);
+            res.status(cachedResponse.status).json(cachedResponse.data);
+        }
+    }
+
+    next();
+};
