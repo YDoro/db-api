e4556f3e685b5c70166471c6214fbc78d0eb483e
yan <yan_mvd@hotmail.com>
2024-12-06 12:13:13 -0300

    feat: :sparkles: accept headers (future authentication)

--- presentation/interfaces/http.ts
+++ presentation/interfaces/http.ts
@@ -1,4 +1,7 @@
+import type { IncomingHttpHeaders } from "node:http";
 export interface Request {
+    headers?: IncomingHttpHeaders;
+    method: string;
     url: string;
     body?: any;
     query?: string;
