b1d131ae415261f55b33c2453c2901698d007aee
yan <yan_mvd@hotmail.com>
2025-01-13 22:13:30 -0300

    fix: 🐛 add params property to Request interface

--- presentation/interfaces/http.ts
+++ presentation/interfaces/http.ts
@@ -1,10 +1,12 @@
 import type { IncomingHttpHeaders } from "node:http";
+
 export interface Request {
     headers?: IncomingHttpHeaders;
     method: string;
     url: string;
     body?: any;
     query?: string;
+    params?: Record<string, string>;
 }
 
 export interface Response {
