a1e3a8d096e59a07fbe274fa2d6d15e2486b8ed7
yan <yan_mvd@hotmail.com>
2025-01-13 22:27:59 -0300

    fix: 🐛 enhance CORS headers to handle preflight requests

--- middlewares/express/cors.ts
+++ middlewares/express/cors.ts
@@ -5,9 +5,11 @@ export class CorsMiddleware implements Middleware {
     async handle(request: Request, response: Response): Promise<void> {
         response.headers = {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept",
-            "Access-Control-Max-Age": "86400",
+            "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Origin, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers",
+            "Access-Control-Expose-Headers": "Content-Length, X-Content-Range",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Max-Age": "86400"
         };
 
         if (request.method === "OPTIONS") {
